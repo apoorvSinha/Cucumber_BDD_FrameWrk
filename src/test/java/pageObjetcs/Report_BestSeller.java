6093e915554e4cd5a0b1167cf32a150cd36b9075
apoorv <apoorvsinha96@gmail.com>
2022-02-16 09:08:25 +0530

    s

--- src/test/java/pageObjetcs/Report_BestSeller.java
+++ src/test/java/pageObjetcs/Report_BestSeller.java
@@ -1,5 +1,7 @@
 package pageObjetcs;
 
+import StepDefs.BaseSteps;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -7,27 +9,36 @@ import org.openqa.selenium.support.PageFactory;
 
 public class Report_BestSeller {
     WebDriver ldriver;
+
+    BaseSteps bs = new BaseSteps();
+
     public Report_BestSeller(WebDriver rdriver) {
         ldriver = rdriver;
         PageFactory.initElements(ldriver, rdriver);
     }
-    @FindBy(xpath="//li//a//p[contains(text(),'Reports')]")
+
+    @FindBy(xpath = "//li//a//p[contains(text(),'Reports')]")
     WebElement ReportMainMeu;
-    @FindBy(xpath="//li//a//p[contains(text(),' Bestsellers')]")
+    @FindBy(xpath = "//li//a//p[contains(text(),' Bestsellers')]")
     WebElement BestSellerSubMenu;
-    @FindBy(xpath="//div[@class='card card-default']//div[@class='card-body']")
+    @FindBy(xpath = "//div[@class='card card-default']//div[@class='card-body']")
     WebElement getTableView;
-    @FindBy(xpath="//tbody//tr//td[1]")
+    @FindBy(xpath = "//tbody//tr//td[1]")
     WebElement getNames;
-    @FindBy(xpath="//tbody//tr//td[3]")
+    @FindBy(xpath = "//tbody//tr//td[3]")
     WebElement getAmount;
 
     public void ClickMainMenuOnReport() {
         ReportMainMeu.click();
     }
-    public void CLickSubMenuReport(){
+
+    public void CLickSubMenuReport() {
         BestSellerSubMenu.click();
     }
 
+    public void getTableIntoView() {
+        bs.js = (JavascriptExecutor) ldriver;
 
+    }
 }
+
